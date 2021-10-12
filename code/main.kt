054200965f721abbc00ecc88e67cab32d6d7ed82
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2021-10-12 15:04:51 +0100

    Update main.kt

--- code/main.kt
+++ code/main.kt
@@ -1,16 +1,15 @@
-import nQueens
 var count = 0
 var c = IntArray(0)
 var f = ""
 
-fun main(args: Array<String>) {
+fun main() {
     for (n in 1..8)
     {
         count = 0
         c = IntArray(n + 1)
         f = ""
-         val nQueens= nQueens()
-        nQueens.nQueensAlgo(1, n)
+         val nQueens= N_Queens()
+        nQueens.N_QueensAlgo(1, n)
         println("For a $n x $n board:")
         println("  Solutions = $count")
         if (count > 0) println("  First is $f")
