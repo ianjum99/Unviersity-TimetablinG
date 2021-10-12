26fcd541a206fee5d3a87938b57ccf6b7da54d13
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2021-10-12 15:05:11 +0100

    Update and rename nQueens.kt to N_Queens.kt

--- code/N_Queens.kt
+++ code/N_Queens.kt
@@ -1,12 +1,12 @@
-class nQueens {
-    fun nQueensAlgo(row: Int, n: Int) {
+class N_Queens {
+    fun N_QueensAlgo(row: Int, n: Int) {
         outer@ for (x in 1..n) {
             for (y in 1..row - 1) {
                 if (c[y] == x) continue@outer
                 if (row - y == Math.abs(x - c[y])) continue@outer
             }
             c[row] = x
-            if (row < n) nQueensAlgo(row + 1, n)
+            if (row < n) N_QueensAlgo(row + 1, n)
             else if (++count == 1) f = c.drop(1).map { it - 1 }.toString()
         }
     }
