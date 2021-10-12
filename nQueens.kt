72f120715b525a27f3f506534f065ee729496151
Ibrahim Anjum <44471479+ianjum99@users.noreply.github.com>
2021-10-12 14:45:59 +0100

    Create nQueens.kt

---
+++ nQueens.kt
@@ -0,0 +1,12 @@
+//actual nQueens algo
+fun nQueens(row: Int, n: Int) {
+    outer@ for (x in 1..n) {
+        for (y in 1..row - 1) {
+            if (c[y] == x) continue@outer
+            if (row - y == Math.abs(x - c[y])) continue@outer           
+        }
+        c[row] = x
+        if (row < n) nQueens(row + 1, n)
+        else if (++count == 1) f = c.drop(1).map { it - 1 }.toString()
+    }
+}
