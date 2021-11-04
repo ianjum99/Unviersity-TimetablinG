f44bd082830a32adadadd2a6b42c1866e6fe85a1
Francesco Romano <56390514+Fra0909@users.noreply.github.com>
2021-11-04 12:25:02 +0000

    Update DataFactory.kt

--- code/DataFactory.kt
+++ code/DataFactory.kt
@@ -5,35 +5,57 @@ import com.google.gson.Gson
 
 private val klaxon = Klaxon()
 
-class DataFactory(elements: Collection<Programme>) : ArrayList<Programme>(elements) {
-    public fun toJson() = Gson().toJson(this)
+class DataFactory(elements: Map<String, Programme>) : HashMap<String, Programme>(elements) {
+    public fun toJson() = klaxon.toJsonString(this)
 
     companion object {
-        public fun fromJson(json: String) = DataFactory(klaxon.parseArray<Programme>(json)!!)
+        public fun fromJson(json: String) = DataFactory (
+                klaxon.parseJsonObject(java.io.StringReader(json)) as Map<String, Programme>
+        )
     }
 }
 
+//    fun createProgramme(programme: Programme) {
+//        this.add(programme)
+//    }
+//
+//    fun deleteProramme(programme: Programme) {
+//        this.remove(programme)
+//    }
+//
+//    fun createModule(programme: Programme, module: Module) {
+//        programme.modules?.add(module)
+//    }
+//
+//    fun deleteModule(programme: Programme, module: Module) {
+//        programme.modules?.remove(module)
+//    }
+//
+//    fun createActivity(module: Module, activity: Activity) {
+//        module.activities?.add(activity)
+//    }
+//
+//    fun deleteActivity(module: Module, activity: Activity) {
+//        module.activities?.remove(activity)
+//    }
+
+
 data class Programme (
-    val name: String,
-    val type: String,
-    val modules: ArrayList<Module>?
+        val name: String,
+        val type: String,
+        val modules: HashMap<String, Module>?
 )
-
 data class Module (
-    val id: Long,
-    val name: String,
-    val compulsory: Boolean,
-
-    @Json(name = "moduleLeader")
-    val moduleLeader: String,
-    val term: Long,
-    val activities: ArrayList<Activity>?
+        val name: String,
+        val compulsory: Boolean,
+        val moduleLeader: String,
+        val term: Long,
+        val activities: HashMap<String, Activity>?
 )
 
-data class Activity(
-    @Json(name = "typeOfActivity")
-    val typeOfActivity: String?,
-    val day: Int?,
-    val time: Int?,
-    val duration: Int?
+data class Activity (
+        val typeOfActivity: String,
+        val day: Long,
+        val time: Long,
+        val duration: Long
 )
