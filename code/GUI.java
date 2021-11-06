eb2c3a5394db53432055d955fd88d7dbe6643b81
Edwin <edwinradvilavicius@yahoo.com>
2021-11-06 00:10:59 +0000

    Added a lot of stuff to the GUI

--- code/GUI.java
+++ code/GUI.java
@@ -1,20 +1,78 @@
-
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class GUI {
 
-    private JLabel uniTimetable;
-    private JLabel Date;
-    private JButton selectionButton;
+    private JLabel timetableLabel;
+    private JLabel dateLabel;
+    private JButton menuButton;
     private JPanel mainPanel;
+    private JLabel mondayLabel;
+    private JLabel tuesdayLabel;
+    private JLabel wednesdayLabel;
+    private JLabel thursdayLabel;
+    private JLabel fridayLabel;
+    private JLabel closeButton;
+    private JFrame frame;
+    private JPanel topBar;
+
+    private int mouseX, mouseY;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Timetable");
-        frame.setContentPane(new GUI().mainPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    GUI window = new GUI();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
+
+    public GUI() {
+        init();
+    }
+
+    private void init() {
+        frame = new JFrame();
+        frame.setContentPane(mainPanel);
+        frame.setUndecorated(true);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        closeButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+
+        topBar.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                frame.setLocation(frame.getX() + e.getX() - mouseX, frame.getY() + e.getY() - mouseY);
+            }
+        });
+        topBar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseX = e.getX();
+                mouseY = e.getY();
+            }
+        });
 
     }
+
+
+    private void createUIComponents() {
+        closeButton = new JLabel((new ImageIcon("images/close.png")));
+    }
 }
+
