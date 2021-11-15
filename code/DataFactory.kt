13007118bd65e13e00d53291ef4c10af627514da
Francesco Romano <56390514+Fra0909@users.noreply.github.com>
2021-11-15 14:29:23 +0000

    Update DataFactory.kt

--- code/DataFactory.kt
+++ code/DataFactory.kt
@@ -1,14 +1,15 @@
 import com.beust.klaxon.*
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import kotlin.random.Random
 
 private val klaxon = Klaxon()
 
 class DataFactory(elements: Collection<Programme>) : ArrayList<Programme>(elements) {
-    fun toJson() = Gson().toJson(this)!!
+    fun toJson() = GsonBuilder().setPrettyPrinting().create().toJson(this)
 
     companion object {
-        fun fromJson(json: String) = DataFactory(klaxon.parseArray<Programme>(json)!!)
+        fun fromJson(json: String) = DataFactory(klaxon.parseArray(json)!!)
     }
 
     fun createProgramme(programme: Programme) {
@@ -25,57 +26,40 @@ class DataFactory(elements: Collection<Programme>) : ArrayList<Programme>(elemen
     }
 
     fun generateModuleId(programme: Programme): String {
-        var moduleId = programme.name.slice(0..3).uppercase()
+        var prefix = programme.name.slice(0..3).uppercase()
         val arrayOfIds = ArrayList<String>()
-        for (module in programme.modules!!) {
-            arrayOfIds.add(module.id.slice(4..7))
-        }
         var suffix = 0
+        programme.modules!!.forEach { module -> arrayOfIds.add(module.id.slice(4..7)) }
         while (true) {
             suffix = Random.nextInt(1000,1999)
             if (!(arrayOfIds.contains(suffix.toString()))) {
-                moduleId += suffix
                 break
             }
         }
-    return moduleId}
+    return prefix+suffix}
 
     fun deleteModule(programme: Programme, module: Module) {
-        programme.modules?.remove(module)
+        programme.modules!!.remove(module)
     }
 
     fun createActivity(module: Module, activity: Activity) {
-
         module.activities?.add(activity)
     }
 
     fun deleteActivity(module: Module, activity: Activity) {
-        module.activities?.remove(activity)
+        module.activities!!.remove(activity)
     }
 
     fun getProgrammeFromActivity(activity: Activity): Programme {
-        return(filter { it -> (it.modules!!.filter { it.activities!!.contains(activity)}).isNotEmpty()}).first()
+        return this.first{programme -> programme.modules!!.any { module -> module.activities!!.contains(activity) } }
     }
 
     fun getModuleFromActivity(activity: Activity): Module {
-        return (this.flatMap { it.modules!! }.filter { module -> module.activities!!.contains(activity) }).first()
+        return (this.flatMap { it.modules!! }.filter { it.activities!!.contains(activity) }).first()
     }
 
     fun getAllActivities(): ArrayList<Activity> {
         return ArrayList(this.flatMap { it.modules!!}.flatMap { it.activities!! })
-    }
-
-    fun checkForClashes(activity: Activity, clashesWith:ArrayList<Activity> = ArrayList()): ArrayList<Activity> {
-        var moduleOfActivity = getModuleFromActivity(activity)
-        (ArrayList(this.getAllActivities().filter {getModuleFromActivity(it).year == moduleOfActivity.year &&
-                                                        getModuleFromActivity(it).term == moduleOfActivity.term &&
-                                                        it.time == activity.time &&
-                                                        it.day == activity.day})).forEach { clash -> clashesWith.add(clash) }
-        if (activity.duration > 1) {
-            println(activity)
-            checkForClashes(Activity(activity.type,activity.day,activity.time+1,activity.duration-1),clashesWith)
-        }
-        return clashesWith
     }
 
     fun getProgrammeInstanceFromString(programmeName: String): Programme {
@@ -85,17 +69,42 @@ class DataFactory(elements: Collection<Programme>) : ArrayList<Programme>(elemen
     fun getModuleInstanceFromString(moduleName: String): Module {
         return (this.flatMap { it.modules!! }.filter { it.name == moduleName }).first()
     }
+
+    fun getActivitiesInSameYearAndTerm(year: Long,term: Long): List<Activity> {
+        return this.flatMap { it.modules!! }.filter { module -> module.year == year && module.term == term  }.flatMap {module-> module.activities!!}
+    }
+
+    fun checkForClashes(activity: Activity,
+                        moduleOfActivity: Module = getModuleFromActivity(activity),
+                        clashesWith:ArrayList<Activity> = ArrayList()): Pair<Activity,ArrayList<Activity>>? {
+
+        val listOfActivities = this.getAllActivities()
+        listOfActivities.remove(activity)
+
+        ArrayList(listOfActivities.filter { currentActivity ->
+            getModuleFromActivity(currentActivity).year == moduleOfActivity.year &&
+            getModuleFromActivity(currentActivity).term == moduleOfActivity.term &&
+            currentActivity.time == activity.time &&
+            currentActivity.day == activity.day}).forEach { clash -> clashesWith.add(clash) }
+
+        if (activity.duration > 1) {
+            checkForClashes(Activity(activity.type,activity.day,activity.time+1,activity.duration-1), moduleOfActivity,clashesWith)
+        }
+
+        if (clashesWith.isNotEmpty()) {
+            return Pair(activity,clashesWith)
+        }
+        return null
+    }
 }
 
-
-
-data class Programme (
+class Programme (
     val name: String,
     val type: String,
     val modules: ArrayList<Module>?
 )
 
-data class Module (
+data class Module(
     var id: String,
     val year: Int,
     val name: String,
